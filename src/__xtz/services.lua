ec20d2da35241885dd5160950fafcf0015d82279
V <cryi@tutanota.com>
2021-12-30 18:46:55 +0100

    http signer, automated releases and improved service organization

--- src/__xtz/services.lua
+++ src/__xtz/services.lua
@@ -1,6 +1,5 @@
 local _appId = am.app.get("id")
 
 return {
-	_appId .. "-xtz-signer" = am.app.get_model("SIGNER_SERVICE_FILE", "__xtz/assets/signer.service"),
-    _appId .. "-xtz-signer-http" = am.app.get_model("SIGNER_HTTP_SERVICE_FILE", "__xtz/assets/signer-http.service")
+	_appId .. "-xtz-signer" = am.app.get_model("SIGNER_SERVICE_FILE", "__xtz/assets/signer.service")
 }
