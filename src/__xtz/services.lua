6bab19171b4f3ce826ad1f5792f59adfee3e5067
V <cryi@tutanota.com>
2021-12-31 20:15:00 +0100

    patched services

--- src/__xtz/services.lua
+++ src/__xtz/services.lua
@@ -1,5 +1,5 @@
 local _appId = am.app.get("id")
 
 return {
-	_appId .. "-xtz-signer" = am.app.get_model("SIGNER_SERVICE_FILE", "__xtz/assets/signer.service")
+	[_appId .. "-xtz-signer"] = am.app.get_model("SIGNER_SERVICE_FILE", "__xtz/assets/signer.service")
 }
