d9b5a78d3d09237e8b9a6073eb801f823dd21443
V <cryi@tutanota.com>
2022-03-23 22:10:09 +0100

    removed xtz prefix from signer service ids

--- src/__xtz/services.lua
+++ src/__xtz/services.lua
@@ -1,12 +1,12 @@
 local _appId = am.app.get("id")
 
 local _services = {
-	[_appId .. "-xtz-signer"] = am.app.get_model("SIGNER_SERVICE_FILE", "__xtz/assets/signer.service")
+	[_appId .. "-signer"] = am.app.get_model("SIGNER_SERVICE_FILE", "__xtz/assets/signer.service")
 }
 
 local _nodeAddr = am.app.get_model("REMOTE_NODE")
 if type(_nodeAddr) == "string" then
-	local _signerTunnelId = am.app.get("id") .. "-xtz-signer-tunnel"
+	local _signerTunnelId = am.app.get("id") .. "-signer-tunnel"
 	_services[_signerTunnelId] = "__xtz/assets/signer-tunnel.service"
 	local _nodeTunnelId = am.app.get("id") .. "-xtz-node-tunnel"
 	_services[_nodeTunnelId] = "__xtz/assets/node-tunnel.service"
