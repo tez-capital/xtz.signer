2d6b1f13ded8b880cb75e3e8402dd582ea0c7409
V <cryi@tutanota.com>
2022-06-19 12:54:43 +0200

    removed udevadm commands

--- src/__xtz/configure.lua
+++ src/__xtz/configure.lua
@@ -22,8 +22,8 @@ local _proc = proc.spawn("/bin/bash", { _tmpFile }, {
 
 fs.remove(_tmpFile)
 ami_assert(_proc.exitcode == 0, "Failed to setup udev rules : " .. _proc.stderrStream:read("a"))
-ami_assert(os.execute("udevadm trigger"), "Failed to run 'udevadm trigger'!")
-ami_assert(os.execute("udevadm control --reload-rules"), "Failed to run 'udevadm control --reload-rules'!")
+-- ami_assert(os.execute("udevadm trigger"), "Failed to run 'udevadm trigger'!")
+-- ami_assert(os.execute("udevadm control --reload-rules"), "Failed to run 'udevadm control --reload-rules'!")
 
 local _ok, _systemctl = am.plugin.safe_get("systemctl")
 ami_assert(_ok, "Failed to load systemctl plugin")
