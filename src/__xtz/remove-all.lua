2c666767c39fcb4fe75e7e7d2c3a3e98ce93858a
V <cryi@tutanota.com>
2023-01-04 23:00:27 +0100

    removed redundant systemctl plugin load

--- src/__xtz/remove-all.lua
+++ src/__xtz/remove-all.lua
@@ -1,8 +1,4 @@
-local _ok, _systemctl = am.plugin.safe_get("systemctl")
-ami_assert(_ok, "Failed to load systemctl plugin")
-
 local _services = require"__xtz.services"
-
 _services.remove_all_services()
 
 log_success("Signer services succesfully removed.")
