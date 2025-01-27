c2130c5dc2e7be3110d6664b44a51819a5fb93e2
v <cryi@tutanota.com>
2025-01-27 10:21:42 +0100

    fixes and support for custom prism configurations

--- src/__xtz/prism/setup.lua
+++ src/__xtz/prism/setup.lua
@@ -22,20 +22,50 @@ local ok, err = prism_key_generator.generate_client("prism/keys/signer", "tezos-
 ami_assert(ok, err or "failed to generate signer key")
 
 -- generate configuration
-local REMOTE_PRISM = am.app.get_configuration("PRISM")
-ami_assert(REMOTE_PRISM, "PRISM not set")
+local PRISM_CONFIGURATION = am.app.get_configuration("PRISM")
+ami_assert(PRISM_CONFIGURATION, "PRISM configuration must be provided")
+
+ami_assert(type(PRISM_CONFIGURATION.remote) == "string", "PRISM remote must be provided")
+
+local are_listening_forwarders_valid_type = type(PRISM_CONFIGURATION.listening_forwarders) == "nil" or
+    type(PRISM_CONFIGURATION.listening_forwarders) == "table"
+ami_assert(are_listening_forwarders_valid_type, "invalid listening_forwarders type")
+local are_connecting_forwarders_valid_type = type(PRISM_CONFIGURATION.connecting_forwarders) == "nil" or
+    type(PRISM_CONFIGURATION.connecting_forwarders) == "table"
+ami_assert(are_connecting_forwarders_valid_type, "invalid connecting_forwarders type")
 
 local prism_configuration = {
-    connecting_forwarders = {
-        default = {
-            connect_to = REMOTE_PRISM,
-            forward_to = am.app.get_model("SIGNER_PORT"),
-            forward_from = am.app.get_model("LOCAL_RPC_PORT"),
-
-            key_path = "prism/keys/signer.prism",
-        }
-    }
+    variables = {
+        default_remote = PRISM_CONFIGURATION.remote,
+        signer_endpoint = am.app.get_configuration("SIGNER_ENDPOINT", "127.0.0.1:20090"),
+        local_rpc_endpoint = "127.0.0.1" .. am.app.get_model("LOCAL_RPC_PORT", "8732")
+    },
+    listening_forwarders = PRISM_CONFIGURATION.listening_forwarders or {},
+    connecting_forwarders = PRISM_CONFIGURATION.connecting_forwarders or {},
 }
+
+ami_assert(type(PRISM_CONFIGURATION.default_forwarder) == "nil" or PRISM_CONFIGURATION.default_forwarder == true or type(PRISM_CONFIGURATION.default_forwarder) == "table",
+    "invalid 'PRISM.default_forwarder' type")
+if type(prism_configuration.connecting_forwarders.default_forwarder) ~= "nil" and type(PRISM_CONFIGURATION.default_forwarder) ~= "nil" then
+    ami_error("PRISM.default_forwarder collides with PRISM.connecting_forwarders")
+end
+
+if PRISM_CONFIGURATION.default_forwarder == true then
+    PRISM_CONFIGURATION.default_forwarder = {
+        signer = true,
+        rpc = true
+    }
+end
+
+if type(PRISM_CONFIGURATION.default_forwarder) == "table" then
+    prism_configuration.connecting_forwarders.default_forwarder = {
+        connect_to = "${default_remote}",
+        forward_to = PRISM_CONFIGURATION.default_forwarder.signer and "${signer_endpoint}" or nil,
+        forward_from = PRISM_CONFIGURATION.default_forwarder.rpc and "${local_rpc_endpoint}" or nil,
+
+        key_path = "prism/keys/signer.prism",
+    }
+end
 
 local ok = fs.safe_write_file("prism/config.hjson", hjson.stringify(prism_configuration))
 ami_assert(ok, "failed to write prism configuration")
