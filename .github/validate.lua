ed7ba3874ce1c5d44ff542a752be5d9c693025e0
v <cryi@tutanota.com>
2025-06-15 20:59:23 +0200

    fix validate

--- .github/validate.lua
+++ .github/validate.lua
@@ -1,6 +1,6 @@
 local hjson = require("hjson")
 -- validate version
-local specs_rwa = fs.read_file("src/specs.hjson")
-local specs = hjson.parse(specs_rwa)
+local specs_raw = fs.read_file("src/specs.json")
+local specs = hjson.parse(specs_raw)
 local version, err = ver.parse(specs.version)
 assert(version, "Failed to parse version: " .. (err or ""))
