eeae5c167183f127da73e24512c04448d4da6e0c
v <cryi@tutanota.com>
2025-12-02 20:25:58 +0100

    fix password handling

--- src/__bin_generated/tezsign.service.template.sh
+++ src/__bin_generated/tezsign.service.template.sh
@@ -3,8 +3,8 @@
 {{#model.TEZSIGN_CONFIGURATION}}
 set -e
 
-export TEZSIGN_UNLOCK_KEYS={{{model.TEZSIGN_CONFIGURATION.unlock_keys}}}
-export TEZSIGN_UNLOCK_PASS={{{model.TEZSIGN_CONFIGURATION.unlock_password}}}
+export TEZSIGN_UNLOCK_KEYS='{{{model.TEZSIGN_CONFIGURATION.unlock_keys}}}'
+export TEZSIGN_UNLOCK_PASS='{{{model.TEZSIGN_CONFIGURATION.unlock_password}}}'
 
 {{{ROOT_DIR}}}/bin/tezsign unlock
 exec {{{ROOT_DIR}}}/bin/tezsign run --listen {{{model.TEZSIGN_CONFIGURATION.listen}}} --no-retry {{{configuration.STARTUP_TEZSIGN_ARGS__CLI_ARGS}}}
